bdb4a82d78a7b8e076dd4e58be40cf880ee1a29e
Mudita Jain <mudita21jain@gmail.com>
2026-05-26 12:53:08 +0530

    `Project structure`

---
+++ backend/app/core/config.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    APP_ENV: str
+
+    API_V1_PREFIX: str
+
+    SECRET_KEY: str
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
---
+++ backend/app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+]
+
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version="1.0.0"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {
+        "message": "QuickCompare API Running"
+    }
+
+@app.get("/health")
+async def health_check():
+    return {
+        "status": "healthy"
+    }
